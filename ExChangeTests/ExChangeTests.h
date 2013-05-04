6ba9bc66cab17f3329440bac8e1d130d0a6ec1e5
chollada <chollada_auy@hotmail.com>
2013-05-04 13:42:09 +0700

    Initial Commit

---
+++ ExChangeTests/ExChangeTests.h
@@ -0,0 +1,13 @@
+//
+//  ExChangeTests.h
+//  ExChangeTests
+//
+//  Created by administrator on 5/4/56 BE.
+//  Copyright (c) 2556 Chollada. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ExChangeTests : SenTestCase
+
+@end
---
+++ ExChangeTests/ExChangeTests.m
@@ -0,0 +1,32 @@
+//
+//  ExChangeTests.m
+//  ExChangeTests
+//
+//  Created by administrator on 5/4/56 BE.
+//  Copyright (c) 2556 Chollada. All rights reserved.
+//
+
+#import "ExChangeTests.h"
+
+@implementation ExChangeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExChangeTests");
+}
+
+@end
