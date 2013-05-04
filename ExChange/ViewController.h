47865d613413282750c6295125176f575d740eb1
chollada <chollada_auy@hotmail.com>
2013-05-04 16:37:38 +0700

    ExChange

--- ExChange/ViewController.h
+++ ExChange/ViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UISwitch *mySwtich;
+- (IBAction)mySwitchAc:(id)sender;
+@property (strong, nonatomic) IBOutlet UITextField *myInput;
+- (IBAction)key:(id)sender;
+@property (strong, nonatomic) IBOutlet UISlider *mySlider;
+@property (strong, nonatomic) IBOutlet UILabel *myRate;
+- (IBAction)myButton:(id)sender;
+@property (strong, nonatomic) IBOutlet UILabel *myOutput;
+- (IBAction)mySliderAc:(id)sender;
 
 @end
--- ExChange/ViewController.m
+++ ExChange/ViewController.m
@@ -26,4 +26,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)mySwitchAc:(id)sender {
+    if (_mySwtich.on==YES) {
+        _myRate.hidden=NO;
+        _myInput.hidden=NO;
+        _mySlider.hidden=NO;
+        _myOutput.hidden=NO;
+              
+    }else{
+        _myRate.hidden=YES;
+        _myInput.hidden=YES;
+        _mySlider.hidden=YES;
+        _myOutput.hidden=YES;
+        _myRate.text=@"30.00";
+    
+}
+}
+- (IBAction)myButton:(id)sender {
+    NSString *outString=@" ";
+    double num1,num2,num3;
+    num1=_myInput.text.doubleValue;
+    num2=_myRate.text.doubleValue;
+    num3=num1*num2;
+    outString=[NSString stringWithFormat:@"%.2f",num3];
+    _myOutput.text=outString;
+    
+}
+- (IBAction)mySliderAc:(id)sender {
+    _myRate.text=[NSString stringWithFormat:@"%.2f",100*
+                _mySlider.value];
+}
+- (IBAction)key:(id)sender {
+    [sender resignFirstResponder];
+}
 @end
